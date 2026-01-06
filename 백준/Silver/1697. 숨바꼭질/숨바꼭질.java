396aea1b6a9ba00a13f1f662ee359f570f20c89c
kookong2 <92318119+kookong2@users.noreply.github.com>
2026-01-07 05:11:10 +0900

    [Silver I] Title: 숨바꼭질, Time: 128 ms, Memory: 18416 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,61 +1,65 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
-public class Main {
-    
-    static final int MAX = 100000;
+public class Main
+{
+	static int N;
+	static int K;
+	
+	static int visited[] = new int[100001];
+	
+	// X-1, X+1
+	// 2*X
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String input = br.readLine();
+		String[] inputs = input.split(" ");
+		
+		N = Integer.valueOf(inputs[0]);
+		K = Integer.valueOf(inputs[1]);
+		
+		int result = bfs(N);
+		System.out.println(result);
+	}
 
-    public static void main(String[] args) throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        
-        int result = bfs(N, K);
-
-        System.out.println(result);
-    }
-    
-    static int bfs(int start, int target) {
-        
-        int[] dist = new int[MAX + 1];                  // 각 위치까지의 시간 기록
-        Arrays.fill(dist, -1);                      // 방문하지 않은 곳은 -1로 초기화
-
-        Queue<Integer> q = new ArrayDeque<>();
-        q.add(start);
-        dist[start] = 0;
-        
-        while(!q.isEmpty()) {
-            int cur = q.poll();                         // 현재 위치를 꺼냄
-            
-            if(cur == target) return dist[cur];         // 목표 위치에 도달했다면 해당 위치까지 걸린 시간 반환
-            
-            int next1 = cur - 1;                        // 이동 경우 1: -1
-            if(next1 >= 0 && dist[next1] == -1) {
-                dist[next1] = dist[cur] + 1;
-                q.add(next1);
-            }
-            
-            int next2 = cur + 1;
-            if(next2 <= MAX && dist[next2] == -1) {     // 이동 경우 2: +1
-                dist[next2] = dist[cur] + 1;
-                q.add(next2);
-            }
-            
-            int next3 = cur * 2;
-            if(next3 <= MAX && dist[next3] == -1) {     // 이동 경우 3: *2
-                dist[next3] = dist[cur] + 1;
-                q.add(next3);
-            }
-        }
-        
-        return -1;
-    }
-
+	private static int  bfs(int node)
+	{
+		Queue<Integer> queue = new LinkedList<Integer>();
+		
+		queue.add(node);
+		int index = node;
+		int n = 0;
+		visited[index] = 1;
+		while (queue.isEmpty() == false)
+		{
+			n = queue.remove();
+			
+			if (n == K)
+			{
+				return visited[n]-1;
+			}
+			
+			if (n-1>=0 && visited[n-1] == 0)
+			{
+				visited[n-1] = visited[n]+1;
+				queue.add(n-1);
+			}
+			if (n+1 <= 100000 && visited[n+1] == 0)
+			{
+				visited[n+1] = visited[n]+1;
+				queue.add(n+1);
+			}
+			if (2*n <= 100000 && visited[2*n] == 0)
+			{
+				visited[2*n] = visited[n] + 1;
+				queue.add(2*n);
+			}
+		}
+		return -1;
+	}
 }
